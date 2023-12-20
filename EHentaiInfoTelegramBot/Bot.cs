321db08cae790905a94dc7be8b40d0bb7ef7ade3
arition <aritionkb@gmail.com>
2023-12-19 20:15:21 -0800

    update packages and runtime

--- EHentaiInfoTelegramBot/Bot.cs
+++ EHentaiInfoTelegramBot/Bot.cs
@@ -5,85 +5,73 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot;
-using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
-namespace EHentaiInfoTelegramBot
+namespace EHentaiInfoTelegramBot;
+
+public class Bot(IConfiguration configuration, ILogger<Bot> logger, IEnumerable<IHentaiInfo> hentaiInfos)
 {
-    public class Bot
+    private bool CheckSecret()
     {
-        private readonly IConfiguration _configuration;
-        private readonly IEnumerable<IHentaiInfo> _hentaiInfos;
-        private readonly ILogger<Bot> _logger;
-
-        public Bot(IConfiguration configuration, ILogger<Bot> logger, IEnumerable<IHentaiInfo> hentaiInfos)
+        if (string.IsNullOrEmpty(configuration["secret"]))
         {
-            _configuration = configuration;
-            _logger = logger;
-            _hentaiInfos = hentaiInfos;
+            logger.LogCritical("Cannot find the bot secret. Please put your bot secret in the json file.");
+            return false;
         }
 
-        private bool CheckSecret()
+        return true;
+    }
+
+    public async Task RunAsync()
+    {
+        if (!CheckSecret()) return;
+
+        try
         {
-            if (string.IsNullOrEmpty(_configuration["secret"]))
+            var bot = new TelegramBotClient(configuration["secret"]);
+            var result = await bot.GetMeAsync();
+            logger.LogInformation($"Running as {result.Username} with id {result.Id}");
+
+            var receiveOptions = new ReceiverOptions
             {
-                _logger.LogCritical("Cannot find the bot secret. Please put your bot secret in the json file.");
-                return false;
-            }
-
-            return true;
-        }
-
-        public async Task RunAsync()
-        {
-            if (!CheckSecret()) return;
-
-            try
-            {
-                var bot = new TelegramBotClient(_configuration["secret"]);
-                var result = await bot.GetMeAsync();
-                _logger.LogInformation($"Running as {result.Username} with id {result.Id}");
-
-                var receiveOptions = new ReceiverOptions
+                AllowedUpdates = new[]
                 {
-                    AllowedUpdates = new[]
+                    UpdateType.Message
+                }
+            };
+
+            var cts = new CancellationTokenSource();
+
+            bot.StartReceiving(async (botClient, update, ctsToken) =>
+                {
+                    if (update.Message?.Text == null) return;
+                    foreach (var hentaiInfo in hentaiInfos)
                     {
-                        UpdateType.Message
+                        if (!hentaiInfo.UrlRegex.IsMatch(update.Message.Text)) continue;
+                        await botClient.SendChatActionAsync(update.Message.Chat.Id, ChatAction.Typing,
+                            cancellationToken: ctsToken);
+                        logger.LogInformation(
+                            $"Receives: {hentaiInfo.UrlRegex.Match(update.Message.Text).Value} from {update.Message.Chat.Id}");
+
+                        using var info =
+                            await hentaiInfo.GetInfoAsync(hentaiInfo.UrlRegex.Match(update.Message.Text).Value);
+                        await botClient.SendPhotoAsync(
+                            update.Message.Chat,
+                            new InputFileStream(info.Cover, "cover.jpg"),
+                            caption: info.ToMarkdown(),
+                            disableNotification: true,
+                            replyToMessageId: update.Message.MessageId,
+                            parseMode: ParseMode.Markdown,
+                            cancellationToken: ctsToken);
                     }
-                };
-
-                var cts = new CancellationTokenSource();
-
-                bot.StartReceiving(async (botClient, update, ctsToken) =>
-                    {
-                        if (update.Message?.Text == null) return;
-                        foreach (var hentaiInfo in _hentaiInfos)
-                        {
-                            if (!hentaiInfo.UrlRegex.IsMatch(update.Message.Text)) continue;
-                            await botClient.SendChatActionAsync(update.Message.Chat.Id, ChatAction.Typing, ctsToken);
-                            _logger.LogInformation(
-                                $"Receives: {hentaiInfo.UrlRegex.Match(update.Message.Text).Value} from {update.Message.Chat.Id}");
-
-                            using var info =
-                                await hentaiInfo.GetInfoAsync(hentaiInfo.UrlRegex.Match(update.Message.Text).Value);
-                            await botClient.SendPhotoAsync(
-                                update.Message.Chat,
-                                new InputMedia(info.Cover, "cover.jpg"),
-                                info.ToMarkdown(),
-                                disableNotification: true,
-                                replyToMessageId: update.Message.MessageId,
-                                parseMode: ParseMode.Markdown,
-                                cancellationToken: ctsToken);
-                        }
-                    },
-                    (_, ex, _) => { _logger.LogError(ex, ex.ToString()); }, receiveOptions, cts.Token);
-
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, e.ToString());
-            }
+                },
+                (_, ex, _) => { logger.LogError(ex, ex.ToString()); }, receiveOptions, cts.Token);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, e.ToString());
         }
     }
 }
